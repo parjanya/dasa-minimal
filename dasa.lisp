a5607380faf2ea3680d8c45f58ac0d195ef5e0b0
Edgard <bikelis@gmail.com>
2020-12-02 18:38:29 -0300

    tries closing connections gracefully

--- dasa.lisp
+++ dasa.lisp
@@ -10,16 +10,25 @@
   (setf (gethash con *connections*)
         (format nil "user-~a" (random 100000))))
 
+(defun handle-close-connection (connection)
+  (let ((message (format nil " .... ~a has left."
+                         (gethash connection *connections*))))
+    (remhash connection *connections*)
+    (loop :for con :being :the :hash-key :of *connections* :do
+      (send con message))))
+
+
 (defparameter *echo-server*
   (lambda (env)
     (let ((ws (make-server env)))
-
       (on :open    ws (lambda () (handle-new-connection ws)))
       (on :message ws (lambda (message)
 			(print ws)
 			(print message)
 			(send ws message)))
-      
+      (on :close   ws (lambda (&key code reason)
+                        (declare (ignore code reason))
+                        (handle-close-connection ws)))
       (lambda (responder)
         (declare (ignore responder))
         (start-connection ws)))))
@@ -29,3 +38,6 @@
 
 ;; WHY DOESN’T THIS WORK?
 ;; (websocket-driver:send-text (car (nth 0 (alexandria:hash-table-alist *connections*))) "tralala")
+
+
+;; (ready-state (car (nth 0 (alexandria:hash-table-alist *connections*))))
